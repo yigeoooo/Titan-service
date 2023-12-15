7bdf84d9ffa7ea972a3cef44ea9de880c8fb0fde
wangjingyi <632084210@qq.com>
2023-12-15 20:15:22 +0800

    環境搭建

---
+++ business/src/main/java/com/titan/BusinessApplication.java
@@ -0,0 +1,17 @@
+package com.titan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @description 業務啟動類
+ * @author yigeoooo
+ * @since date 2023/12/15
+ */
+@SpringBootApplication
+public class BusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
+}
